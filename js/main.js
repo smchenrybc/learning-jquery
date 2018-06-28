eee30d2d7da4a34f3fcd5086a415c3821bcfc5b4
Sean McHenry <sean@browndesign.com>
2018-06-28 16:44:03 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/**
+ * main.js
+ */
+
+jQuery(function($) {
+  // click event
+  $(".main a").on("click", function(event) {
+    console.log('The main div link was clicked!');
+  });
+
+  // get the height of the first paragraph
+  var height = $('.main :first-child').height();
+
+  // append a div with the height contained in it
+  $(".main .first").after('<div class="info p2 mbp"></div>');
+  $(".info").append('<span>The paragraph above is <h1 class="inline mt0 mx1">' + height + '</h1> pixels tall</span>');
+});
