4fc395dae192cf6dec24675d7911a62075b3dcb5
Sean McHenry <sean@browndesign.com>
2018-06-29 14:53:46 -0400

    Added boxes with animation

--- js/main.js
+++ js/main.js
@@ -3,11 +3,6 @@
  */
 
 jQuery(function($) {
-  // click event
-  $(".main a").on("click", function(event) {
-    console.log('The main div link was clicked!');
-  });
-
   // get the height of the first paragraph
   var height = $('.main :first-child').height();
 
@@ -16,6 +11,42 @@ jQuery(function($) {
   $("#block-1 .block-inner").append('<span class="bold">The paragraph above is <h1 class="inline mt0 mx1">' + height + '</h1> pixels tall</span>');
 
 
-  // animate one of the buttons
-  $("#block-4 a").text('Click me!');
+  // set up animation for each box
+  function animateBox() {
+    box = $(this);
+
+    if ( box.hasClass("clicked") ) {
+      // animate it
+      box.animate({
+        "bottom": 0
+      });
+
+      // remove clicked class
+      box.removeClass("clicked");
+    } else {
+      // animate it
+      box.animate({
+        "bottom": 100
+      });
+
+      // add clicked class
+      box.addClass('clicked');
+    }
+  }
+  // animation for all boxes to
+  // move at the same time
+  function animateBoxes(e) {
+    e.preventDefault();
+
+    var boxes = $('.boxes .box');
+
+    boxes.each(animateBox);
+  }
+
+  // animate a box if it's clicked
+  $('.boxes .box').on("click", animateBox);
+
+  // animate all boxes if the button
+  // below them is clicked
+  $('#box-area .btn').on("click", animateBoxes);
 });
